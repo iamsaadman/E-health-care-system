bc49d609bce8d1c2a48e9189cafa7446430b3171
Muhammad Saadman Sakib Hossain <saadmansakib34@gmail.com>
2024-12-23 19:39:25 +0600

    Adding Homepage using Talwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+       Baloo: "'Baloo Bhai 2' , serif" ,
+      }
+    },
+  },
+  plugins: [],
+}
+
