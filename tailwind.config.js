541a9be658670da3a6bc12f3ebd4b9445b761726
Muhammad Saadman Sakib Hossain Hossain <saadmansakib34@gmail.com>
2024-12-27 03:00:58 +0600

    NEW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./*.{html,js}"], // Ensure Tailwind scans your HTML and JS files
   theme: {
     extend: {
       fontFamily: {
-       Baloo: "'Baloo Bhai 2' , serif" ,
-      }
+        my: ["'Baloo Bhai 2'", "'League Spartan'", "'Poppins'", "sans-serif"], // Add all fonts
+      },
     },
   },
   plugins: [],
-}
-
+};
